53ec70ffe0bbc764c415135bcddc477c0903f260
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-07-26 13:05:47 +0300

    Add React Hooks talk

--- themes.js
+++ themes.js
@@ -3,9 +3,16 @@ import future from '@mdx-deck/themes/future';
 import syntaxHighlighterPrism from '@mdx-deck/themes/syntax-highlighter-prism';
 
 const theme = {
+	fontSizes: [
+    12, 14, 16, 20, 24, 32, 48, 64
+  ].map(x => x * 0.6),
+	lineHeights: {
+		body: 1.5,
+		heading: 1.125,
+	},
 	styles: {
 		code: {
-			fontSize: '0.75em'
+			fontSize: '0.8em'
 		}
 	}
 };
