f51c9af1faf8c796902b7df09ad23f6608520834
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-07-26 11:06:37 +0300

    Update to latest mdx deck

---
+++ themes.js
@@ -0,0 +1,13 @@
+import aspect from '@mdx-deck/themes/aspect';
+import future from '@mdx-deck/themes/future';
+import syntaxHighlighterPrism from '@mdx-deck/themes/syntax-highlighter-prism';
+
+const theme = {
+	styles: {
+		code: {
+			fontSize: '0.75em'
+		}
+	}
+};
+
+export default [aspect, future, syntaxHighlighterPrism, theme];
